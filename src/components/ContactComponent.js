5bb48c79a67f5a16010619cc76431bc0030cc1c3
Ruthless <ruth@jetpackworkflow.com>
2020-03-27 21:15:16 -0400

    Breadcrumbs

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 function Contact(props) {
     return (
         <div className="container">
             <div className="row">
                 <div className="col">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                    </Breadcrumb>
                     <h2>Contact Us</h2>
                     <hr />
                 </div>
             </div>
-
             <div className="row row-content align-items-center">
                 <div className="col-sm-4">
                     <h5>Our Address</h5>
@@ -21,6 +28,7 @@ function Contact(props) {
                 <div className="col">
                     <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone"></i> 1-206-555-1234</a><br />
                     <a role="button" className="btn btn-link" href="mailto:fakeemail@fakeemail.co"><i className="fa fa-envelope-o"></i> campsites@nucamp.co</a>
+                </div>
                 </div>
             </div>
         </div>
