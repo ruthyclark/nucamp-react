03d111f4935bed9d5f167f49e112d56a1e05e486
Ruthless <ruth@jetpackworkflow.com>
2020-04-10 22:02:22 -0400

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -29,7 +29,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render () {
@@ -108,7 +108,7 @@ function RenderCampsite({campsite}) {
     )
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     
     if (comments) {
         return (
@@ -127,7 +127,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         );
                     })}
                 </Stagger>
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     };
@@ -175,7 +175,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                             <RenderCampsite campsite={props.campsite} />
                             <RenderComments 
                                 comments={props.comments}
-                                addComment={props.addComment}
+                                postComment={props.postComment}    
                                 campsiteId={props.campsite.id}
                              />
                     </div>
